f71c813d83bb2e87f1f8f4caa16435a637aa5e6c
ResoluteBoar <75696936+ResoluteBoar@users.noreply.github.com>
2024-12-31 12:43:31 +0300

    Add files via upload

---
+++ StudentStorage.java
@@ -0,0 +1,93 @@
+package students;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StudentStorage {
+
+    private Map<Long, Student> studentStorageMap = new HashMap<>();
+    private StudentSurnameStorage studentSurnameStorage = new StudentSurnameStorage();
+    private Long currentId = 0L;
+
+    public Long createStudent(Student student){
+        try{
+            Long nextId = getNextId();
+            student.setId(nextId);
+            studentStorageMap.put(nextId,student);
+            studentSurnameStorage.studentCreated(nextId, student.getSurname());
+            return nextId;
+        }
+        catch (NumberFormatException e){
+            System.out.println("Введённые данные некорректны");
+            return null;
+        }
+    }
+    public boolean updateStudent(Long id, Student student) {
+        if (!studentStorageMap.containsKey(id)){
+            return false;
+        } else{
+            String newSurname = student.getSurname();
+            String oldSurname = studentStorageMap.get(id).getSurname();
+            studentSurnameStorage.studentUpdated(id, oldSurname, newSurname);
+            studentStorageMap.put(id, student);
+            return true;
+        }
+    }
+    public Long getNextId(){
+        currentId = currentId +1;
+        return currentId;
+    }
+    public void printAll(){
+        System.out.println(studentStorageMap);
+    }
+    public boolean deleteStudent(Long id){
+        Student removed = studentStorageMap.remove(id);
+        if (removed != null){
+            String surname = removed.getSurname();
+            studentSurnameStorage.studentDeleted(id, surname);
+        }
+        return removed != null;
+    }
+    public void printMap(Map<String, Long> data){
+        data.entrySet().stream().forEach(e -> {
+            System.out.println(e.getKey() + " - " + e.getValue());
+        });
+    }
+    public Map<String,Long> getCountByCourse(){
+        Map<String, Long> res = new HashMap<>();
+        for (Student student : studentStorageMap.values()){
+            String key = student.getCourse();
+            Long count = res.getOrDefault(key, 0L);
+            count++;
+            res.put(key,count);
+        }
+        return res;
+    }
+    public Map<String,Long> getCountByCity(){
+        Map<String, Long> res = studentStorageMap.values().stream()
+                .collect(Collectors.toMap(
+                        student -> student.getCity(),
+                        student -> 1L,
+                        (count1, count2) -> count1 + count2
+                ));
+        return res;
+    }
+    public void search(String surname){
+        String[] surnames = surname.split(",");
+        if (Objects.equals(surnames[0], "") && surnames.length==1){
+            for (Map.Entry<Long, Student> entry : studentStorageMap.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        } else if (surnames.length == 1) {
+            Set<Long> students = studentSurnameStorage.getStudentBySurnamesLessOrEqualThan(surname);
+
+            for (Long studentId : students){
+                Student student = studentStorageMap.get(studentId);
+                System.out.println(studentId);
+            }
+        }
+    }
+}
---
+++ StudentsCommandHandler.java
@@ -0,0 +1,101 @@
+package students;
+
+import java.util.Map;
+
+public class StudentsCommandHandler {
+
+    private StudentStorage studentStorage = new StudentStorage();
+    boolean hasError = false;
+
+    public void processCommand(Command command) throws Exception {
+        Action action = command.getAction();
+        switch (action) {
+            case CREATE -> {
+                processCreateCommand(command);
+                break;
+            }
+            case UPDATE -> {
+                processUpdateCommand(command);
+                break;
+            }
+            case DELETE -> {
+                processDeleteCommand(command);
+                break;
+            }
+            case STATS_BY_COURSE -> {
+                processStatsByCourceCommand(command);
+                break;
+            }
+            case STATS_BY_CITY -> {
+                processStatsByCityCommand(command);
+                break;
+            }
+            case SEARCH -> {
+                processSearchCommand(command);
+                break;
+            }
+            default -> {
+                System.out.println("Дейтсвие "+ action + " не поддерживается");
+            }
+        }
+        if (!hasError) {
+            System.out.println("Обработка команд. Действие: " + command.getAction().name() + " , данные: " + command.getData());
+        }
+
+    }
+    private void processCreateCommand(Command command) throws Exception {
+        String data = command.getData();
+        String[] dataArray = data.split(",");
+        Student student = new Student();
+        try{
+            student.setSurname(dataArray[0]);
+            student.setName(dataArray[1]);
+            student.setCourse(dataArray[2]);
+            student.setCity(dataArray[3]);
+            student.setAge(Integer.valueOf(dataArray[4]));
+            studentStorage.createStudent(student);
+            studentStorage.printAll();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new Exception("Неверный размер массива");
+        }
+    }
+    public void processUpdateCommand(Command command) throws Exception {
+        String data = command.getData();
+        String[] dataArray = data.split(",");
+        Student student = new Student();
+
+        try {
+            Long id = Long.valueOf(dataArray[0]);
+
+            student.setName(dataArray[1]);
+            student.setSurname(dataArray[2]);
+            student.setCourse(dataArray[3]);
+            student.setCity(dataArray[4]);
+            student.setAge(Integer.valueOf(dataArray[5]));
+
+            studentStorage.updateStudent(id, student);
+            studentStorage.printAll();
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new Exception("Неверный размер массива");
+        }
+
+    }
+    public void processDeleteCommand(Command command){
+        String data = command.getData();
+        Long id = Long.valueOf(data);
+    }
+    public void processStatsByCourceCommand(Command command){
+        Map<String,  Long> data = studentStorage.getCountByCourse();
+        studentStorage.printMap(data);
+    }
+    public void processStatsByCityCommand(Command command){
+        Map<String,  Long> data = studentStorage.getCountByCity();
+        studentStorage.printMap(data);
+    }
+    private void processSearchCommand(Command command){
+        String surname = command.getData();
+        studentStorage.search(surname);
+    }
+
+}
